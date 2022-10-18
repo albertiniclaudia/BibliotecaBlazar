7127fad1531f76925171bcacdc4871e36eb7d813
albertiniclaudia <claudia.albertini@live.it>
2022-10-18 15:49:07 +0200

    Claudia pome

--- BibliotecaProject/BibliotecaProject/Controllers/HomeController.cs
+++ BibliotecaProject/BibliotecaProject/Controllers/HomeController.cs
@@ -45,7 +45,15 @@ namespace BibliotecaProject.Controllers
 
         }
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult DetailBook()
+        {
+            return View();
+
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
